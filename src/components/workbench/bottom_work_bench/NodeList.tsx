c2a47ef832307a78becb4e74a2216399deb47f46
Clarence Jordan <clarencejordan087@gmail.com>
2024-07-31 01:40:55 -0500

    btn update

---
+++ src/components/workbench/bottom_work_bench/NodeList.tsx
@@ -0,0 +1,17 @@
+const NodeList = () => {
+  return (
+    <ul className="menu bg-base-200 rounded-box w-56">
+      <li>
+        <button>Default | 1 edge</button>
+      </li>
+      <li>
+        <button>Default | 2 edge</button>
+      </li>
+      <li>
+        <button>Default | 3 edge</button>
+      </li>
+    </ul>
+  );
+};
+
+export default NodeList;
